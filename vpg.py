350797ef99bf8405a95f8b8b7e1be3292ce41d11
Philippe Proctor <philipproctor44@gmail.com>
2021-07-19 12:18:19 -0700

    Added render capability to show the model after being trained, also added plotting of the logged results

--- vpg.py
+++ vpg.py
@@ -85,7 +85,7 @@ class VPGBuffer:
 
 
 def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(),  seed=0, 
-        steps_per_epoch=4000, epochs=50, gamma=0.99, pi_lr=3e-4,
+        steps_per_epoch=4000, epochs=50, gamma=0.99, pi_lr=3e-4, render=False,
         vf_lr=1e-3, train_v_iters=80, lam=0.97, max_ep_len=1000,
         logger_kwargs=dict(), save_freq=10):
     """
@@ -248,6 +248,8 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(),  seed=0,
     # Main loop: collect experience in env and update/log each epoch
     for epoch in range(epochs):
         for t in range(local_steps_per_epoch):
+            if render and proc_id() == 0 and (epoch == epochs-1):
+                env.render()
             a, v, logp = ac.step(torch.as_tensor(o, dtype=torch.float32))
 
             next_o, r, d, _ = env.step(a)
@@ -284,6 +286,8 @@ def vpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(),  seed=0,
         if (epoch % save_freq == 0) or (epoch == epochs-1):
             logger.save_state({'env': env}, None)
 
+        if (epoch == epochs-1) and proc_id() == 0:
+            env.close()
         # Perform VPG update!
         update()
 
@@ -310,10 +314,11 @@ if __name__ == '__main__':
     parser.add_argument('--l', type=int, default=2)
     parser.add_argument('--gamma', type=float, default=0.99)
     parser.add_argument('--seed', '-s', type=int, default=0)
-    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--cpu', type=int, default=2)
     parser.add_argument('--steps', type=int, default=4000)
-    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--epochs', type=int, default=150)
     parser.add_argument('--exp_name', type=str, default='vpg')
+    parser.add_argument('--render', type=bool, default=False)
     args = parser.parse_args()
 
     mpi_fork(args.cpu)  # run parallel code with mpi
@@ -323,5 +328,5 @@ if __name__ == '__main__':
 
     vpg(lambda : gym.make(args.env), actor_critic=core.MLPActorCritic,
         ac_kwargs=dict(hidden_sizes=[args.hid]*args.l), gamma=args.gamma, 
-        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
+        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,render=args.render,
         logger_kwargs=logger_kwargs)
